9ac3377ca7a4a4ce00a32e583011d9fe3a5a421e
Guilherme Rocha <guilherme.rochalima17@gmail.com>
2023-08-19 08:50:57 -0300

    Remoção Rotas Default Template

--- Avaliacao_Laravel_Dix/resources/views/news/edit.blade.php
+++ Avaliacao_Laravel_Dix/resources/views/news/edit.blade.php
@@ -10,7 +10,7 @@
                     </div>
             @endif
             <div class="card-header">
-                <h4 class="card-title">{{ __('Edit New') }}</h4>
+                <h4 class="card-title">{{ __('Editar Noticia') }}</h4>
             </div>
             <form method="post" action="{{ route('news.update', $new->id) }}" autocomplete="off">
                 <div class="card-body">
@@ -21,19 +21,19 @@
 
                         <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
                             <label>{{ __('Title') }}</label>
-                            <input type="text" name="title" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" placeholder="{{ __('Title') }}" value="{{ $new->title }}">
+                            <input type="text" name="title" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" placeholder="{{ __('Titulo') }}" value="{{ $new->title }}">
                             @include('alerts.feedback', ['field' => 'title'])
                         </div>
 
                         <div class="form-group{{ $errors->has('description') ? ' has-danger' : '' }}">
-                            <label>{{ __('Description') }}</label>
-                            <input type="text" name="description" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" placeholder="{{ __('Description') }}" value="{{ $new->description }}">
+                            <label>{{ __('Descrição') }}</label>
+                            <input type="text" name="description" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" placeholder="{{ __('Descrição') }}" value="{{ $new->description }}">
                             @include('alerts.feedback', ['field' => 'description'])
                         </div>
 
                         <div class="form-group{{ $errors->has('content') ? ' has-danger' : '' }}">
-                            <label>{{ __('Content') }}</label>
-                            <textarea name="content" id="content" cols="57" rows="50"  class="form-control{{ $errors->has('content') ? ' is-invalid' : '' }}" placeholder="{{ __('Content') }}">{{ $new->content }}</textarea>
+                            <label>{{ __('Conteudo') }}</label>
+                            <textarea name="content" id="content" cols="57" rows="50"  class="form-control{{ $errors->has('content') ? ' is-invalid' : '' }}" placeholder="{{ __('Conteudo') }}">{{ $new->content }}</textarea>
                             @include('alerts.feedback', ['field' => 'content'])
                         </div>
                 </div>
